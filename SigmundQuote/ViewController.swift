aef8547cc305b5d85bd599c5f75b46211d01798f
Masahiro Aoki <rafe.kun@gmail.com>
2018-11-19 18:31:03 +0900

    change randomNumber to 乱数生成 and freudQuoteArray to 引用の配列

--- SigmundQuote/ViewController.swift
+++ SigmundQuote/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var fukidashi: UIImageView!
-    let freudQuoteArray = ["Fukidashi1", "Fukidashi2", "Fukidashi3", "Fukidashi4", "Fukidashi5"]
-    var randomNumber: Int = 0
+    let 引用の配列 = ["Fukidashi1", "Fukidashi2", "Fukidashi3", "Fukidashi4", "Fukidashi5"]
+    var 乱数生成: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +24,8 @@ class ViewController: UIViewController {
         
     }
     func quoteRoll () {
-        randomNumber = Int.random(in: 0 ... 4)
-        fukidashi.image = UIImage(named: freudQuoteArray[randomNumber])
+        乱数生成 = Int.random(in: 0 ... 4)
+        fukidashi.image = UIImage(named: 引用の配列[乱数生成])
     }
     
 }
